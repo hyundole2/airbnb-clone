1eed23f3f7e9e9afa092fc74976870f931eae941
hyundole2 <56394805+hyundole2@users.noreply.github.com>
2021-10-14 17:18:15 +0900

    211014 #5.0

--- core/models.py
+++ core/models.py
@@ -7,8 +7,8 @@ class TimeStampedModel(models.Model):
 
     """Time Stamped Model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True) #auto_now_add -- 모델이 생설될 때 시간과 날짜를 구함
+    updated = models.DateTimeField(auto_now=True) #auto_now_add -- 모델이 업데이트될 때 시간과 날짜를 구함
 
     class Meta:
         abstract = True
